161f0b6c835af8d296282ddc0054d138c0b9b869
VeselinovStf <chofexx@gmail.com>
2018-07-23 17:59:53 +0300

    Changing IDisplayable to read from List

--- LifeSim/LifeSim.Core/Engine/Menu/Contracts/IDisplayable.cs
+++ LifeSim/LifeSim.Core/Engine/Menu/Contracts/IDisplayable.cs
@@ -1,7 +1,12 @@
-﻿namespace LifeSim.Core.Engine.Menu.Contracts
+﻿using System.Collections.Generic;
+
+namespace LifeSim.Core.Engine.Menu.Contracts
 {
+    /// <summary>
+    /// Gives ability to display line of text (string) to the user.
+    /// </summary>
     public interface IDisplayable
     {
-        void DisplayLine(string line);
+        void DisplayLine(IList<string> content);
     }
 }
