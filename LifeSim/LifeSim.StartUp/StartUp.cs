ae8241b25a132748e1403804af3a616568beb13e
VeselinovStf <chofexx@gmail.com>
2018-07-23 16:58:06 +0300

    Pushing demo start project structure see Readme.md for more info

---
+++ LifeSim/LifeSim.StartUp/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LifeSim.StartUp
+{
+    /// <summary>
+    /// Console Client start point of "Life Simulator"
+    /// </summary>
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("LifeSim is still under development!");
+        }
+    }
+}
---
+++ LifeSim/LifeSim_V01_01.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LifeSim_V01_01.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LifeSim_V01_01.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("04247be1-7265-4062-8d40-6d0bad9fb3bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
