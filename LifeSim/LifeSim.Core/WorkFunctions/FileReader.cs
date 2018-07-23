ab51e5119ee00df7da74c9530fd5d9a20ffe3c6e
VeselinovStf <chofexx@gmail.com>
2018-07-23 18:23:49 +0300

    adding ReadFile functionalyty

--- LifeSim/LifeSim.Core/WorkFunctions/FileReader.cs
+++ LifeSim/LifeSim.Core/WorkFunctions/FileReader.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using LifeSim.Core.Contracts;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace LifeSim.Core.WorkFunctions
 {
-    class FileReader
+    public class FileReader : IReadable
     {
+        public IList<string> ReadFile(string path)
+        {
+            var strLines = new List<string>();
+
+            StreamReader reader = new StreamReader(path);
+            using (reader)
+            {
+                string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    strLines.Add(line);
+
+                    line = reader.ReadLine();
+                }
+            }
+
+            return strLines;
+        }
     }
 }
