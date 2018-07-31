34c3d5083898af113d3f22028e0e08951987a912
eXtraGoZ <stranged96@gmail.com>
2018-07-31 15:43:44 +0300

    .gitIgnore
    .gitIgnore & Constructor for Player Class

--- LifeSim/LifeSim.Player/Player.cs
+++ LifeSim/LifeSim.Player/Player.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using LifeSim.Player.Enums;
 using LifeSim.Player.Models;
 
 namespace LifeSim.Player
 {
     public class Player : AbstractPlayer
     {
+        public Player(string firstname, string lastname, GenderType gender, Birthplaces birthplace) : base(firstname, lastname, gender, birthplace)
+        {
 
+        }
     }
 }
