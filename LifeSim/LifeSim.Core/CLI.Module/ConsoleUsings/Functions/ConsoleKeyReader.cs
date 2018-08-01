da8fdad9992172adc89acec636b9db71a489ab10
Stefan Veselinov <chofexx@gmail.com>
2018-08-01 14:18:00 +0300

    read key function to CLI.Module
    Interface + method for read key -> return type is considerable !

--- LifeSim/LifeSim.Core/CLI.Module/ConsoleUsings/Functions/ConsoleKeyReader.cs
+++ LifeSim/LifeSim.Core/CLI.Module/ConsoleUsings/Functions/ConsoleKeyReader.cs
@@ -1,6 +1,15 @@
-﻿namespace LifeSim.Core.CLI.Module.ConsoleUsings.Functions
+﻿using System;
+using LifeSim.Core.CLI.Module.ConsoleUsings.Contracts;
+
+namespace LifeSim.Core.CLI.Module.ConsoleUsings.Functions
 {
-    public class ConsoleKeyReader
+    public class ConsoleKeyReader : IConsoleReadKey
     {
+        public ConsoleKeyInfo ReadKey()
+        {
+            ConsoleKeyInfo cki = Console.ReadKey();
+
+            return cki;
+        }
     }
 }
