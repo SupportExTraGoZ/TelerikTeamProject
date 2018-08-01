b76412b33dfb092f96b90b636f2f5277dace0f11
VeselinovStf <chofexx@gmail.com>
2018-08-01 10:21:26 +0300

    Setting up simple singleton implementation in engine

--- LifeSim/LifeSim.Core/Engine/Core/Engine.cs
+++ LifeSim/LifeSim.Core/Engine/Core/Engine.cs
@@ -2,6 +2,29 @@
 {
     public class Engine
     {
-        //Something funni here :)
+        private static readonly Engine engineInstance;
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        
+
+        private Engine()
+        {
+
+        }
+
+        public static Engine Instance
+        {
+            get
+            {
+                if (engineInstance == null)
+                {
+                    return new Engine();
+                }
+
+                return engineInstance;
+            }
+        }
+
     }
 }
