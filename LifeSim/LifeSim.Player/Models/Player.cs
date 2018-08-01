8129ef2646611dc82e158220ad923a7bb8ef050c
VeselinovStf <chofexx@gmail.com>
2018-08-01 10:43:22 +0300

    pushing current state

--- LifeSim/LifeSim.Player/Models/Player.cs
+++ LifeSim/LifeSim.Player/Models/Player.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using LifeSim.Player.Enums;
 using LifeSim.Player.Contracts;
-using LifeSim.Player.Randomizer;
+using LifeSim.Player.Randomizer.Models;
 using LifeSim.Player.Models;
+using LifeSim.Player.Randomizer.Contracts;
 
 namespace LifeSim.Player.Models
 {
     public class Player : AbstractPlayer
     {
-        public Parent Father { get; private set; }
-        public Parent Mother { get; private set; }
-        public Player(string firstname, string lastname, GenderType gender, Birthplaces birthplace) : base(firstname, lastname, gender, birthplace)
+        public Parent Father { get; }
+        public Parent Mother { get; }
+        public Player(string firstname, string lastname, GenderType gender, Birthplaces birthplace, IFamilyGenerator familyGenerator) : base(firstname, lastname, gender, birthplace)
         {
-            var familyNames = FamilyGenerator.ChooseParentNames();
+            var familyNames = familyGenerator.ChooseParentNames();
             var fatherName = familyNames[0].Split();
             var motherName = familyNames[1].Split();
-            this.Father = new Parent(fatherName[0], fatherName[1], GenderType.Male, FamilyGenerator.ChooseBirthplace());
-            this.Mother = new Parent(motherName[0], motherName[1], GenderType.Female, FamilyGenerator.ChooseBirthplace());
+            this.Father = new Parent(fatherName[0], fatherName[1], GenderType.Male, familyGenerator.ChooseBirthplace());
+            this.Mother = new Parent(motherName[0], motherName[1], GenderType.Female, familyGenerator.ChooseBirthplace());
         }
     }
 }
---
+++ LifeSim/LifeSim.Player/Randomizer/Models/FamilyGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LifeSim.Player.Enums;
+using LifeSim.Player.Randomizer.Contracts;
+namespace LifeSim.Player.Randomizer.Models
+{
+    public class FamilyGenerator : IFamilyGenerator
+    {
+        private string[] FatherNames = new string[] { "John Doesh", "Vasilis Petrovich", "Ivan Kitukovich", "Pedro Lamas", "Gosho Toshov" };
+        private string[] MotherNames = new string[] { "Mariya Doesh", "Petranka Petrovich", "Ivanka Kitukovich", "Milla Lamas", "Pesha Toshova" };
+        private readonly Random GetRandom = new Random();
+
+        public string[] ChooseParentNames()
+        {
+            string fatherName, motherName;
+            fatherName = FatherNames[GetRandom.Next(0, FatherNames.Length - 1)];
+            motherName = MotherNames[GetRandom.Next(0, FatherNames.Length - 1)];
+            return new string[] { fatherName, motherName };
+        }
+
+        public Birthplaces ChooseBirthplace()
+        {
+            return (Birthplaces)GetRandom.Next(0, 3);
+        }
+
+        public int ChooseAge()
+        {
+            return GetRandom.Next(30, 50);
+        }
+    }
+}
