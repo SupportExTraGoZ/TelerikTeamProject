b6871a402843574eaa26ac7afe4e82768fe9fdb1
eXtraGoZ <stranged96@gmail.com>
2018-08-09 16:57:22 +0300

    Small Update for OptionsContainer

--- LifeSim/LifeSim.Player/Options/Models/OptionsContainer.cs
+++ LifeSim/LifeSim.Player/Options/Models/OptionsContainer.cs
@@ -9,22 +9,39 @@ namespace LifeSim.Player.Options
 {
     public class OptionsContainer : IOptionsContainer
     {
-        private Dictionary<string, PlayerProgress> options;
+        private Dictionary<string, CustomTuple> options;
 
         public OptionsContainer()
         {
-            options = new Dictionary<string, PlayerProgress>
+            options = new Dictionary<string, CustomTuple>
             {
-                { "1. Age Up (ageup)", PlayerProgress.NewBorn }
+                { "Age Up (ageup)", new CustomTuple(PlayerProgress.NewBorn, true, true, false) }
             };
         }
 
         public IEnumerable<string> CurrentStageOptions(PlayerProgress playerProgress)
         {
-            foreach (var option in options.Where(x => x.Value == playerProgress))
+            foreach (var option in options
+                     .Where(x => x.Value.playerProgress == playerProgress)
+                     .Where(x => x.Value.isUnlocked)
+                     .Where(x => x.Value.canBeUsedManyTimes || !x.Value.isUsed))
             {
                 yield return option.Key;
             }
+        }
+    }
+
+    class CustomTuple
+    {
+        internal PlayerProgress playerProgress;
+        internal bool isUnlocked, canBeUsedManyTimes, isUsed;
+
+        public CustomTuple(PlayerProgress playerProgress, bool isUnlocked, bool canBeUsedManyTimes, bool isUsed)
+        {
+            this.playerProgress = playerProgress;
+            this.isUnlocked = isUnlocked;
+            this.canBeUsedManyTimes = canBeUsedManyTimes;
+            this.isUsed = isUsed;
         }
     }
 }
