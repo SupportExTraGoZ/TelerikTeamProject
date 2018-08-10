39e14be8af67f2cd4a4b42664abf05a277626129
Angel <angelmizinov@abv.bg>
2018-08-10 09:27:13 +0300

    adding KinderGarten

---
+++ LifeSim/LifeSim.Establishments/Education/Models/KinderGarten/Contract/IKinderGarten.cs
@@ -0,0 +1,10 @@
+﻿using LifeSim.Establishments.Education.Contracts;
+
+namespace LifeSim.Establishments.Education.Models.KinderGarten.Contract
+{
+    public interface IKinderGarten : IEducationalInstitute
+    {
+
+    }
+
+}
---
+++ LifeSim/LifeSim.Establishments/Education/Models/KinderGarten/Models/KinderGarten.cs
@@ -0,0 +1,12 @@
+﻿using LifeSim.Establishments.Education.Models.KinderGarten.Contract;
+
+namespace LifeSim.Establishments.Education.Models.KinderGarten.Models
+{
+    public class KinderGarten : EducationalInstitute , IKinderGarten
+    {
+        public KinderGarten(string name,int startYear, int graduateYear) : base(name,startYear,graduateYear)
+        {
+
+        }
+    }
+}
