b7bb5a305d1b1c5f219cdf9892af0c56d197c93f
eXtraGoZ <stranged96@gmail.com>
2018-08-11 11:49:01 +0300

    Sorting files

--- LifeSim/LifeSim.Core/Engine/Commands/Actions/Schools/EndHighSchoolCommand.cs
+++ LifeSim/LifeSim.Core/Engine/Commands/Actions/Schools/EndHighSchoolCommand.cs
@@ -4,13 +4,13 @@ using LifeSim.Core.Engine.Commands.Contracts;
 using LifeSim.Core.Engine.Core.Contracts;
 using System.Text;
 
-namespace LifeSim.Core.Engine.Commands.Actions
+namespace LifeSim.Core.Engine.Commands.Actions.Schools
 {
-    public class EndPrimarySchoolCommand : ICommand
+    public class EndHighSchoolCommand : ICommand
     {
         private readonly IEngine engine;
 
-        public EndPrimarySchoolCommand(IEngine engine)
+        public EndHighSchoolCommand(IEngine engine)
         {
             this.engine = engine;
         }
@@ -18,26 +18,26 @@ namespace LifeSim.Core.Engine.Commands.Actions
         public string Execute(IList<string> parameters)
         {
             var examsPercent = this.engine.NumberGenerator.RandomChance();
-            if (this.engine.Player.IsTakingLessons) this.engine.Player.IsSuccessfulAtPrimarySchool = true;
-            else if (examsPercent < 50) this.engine.Player.IsSuccessfulAtPrimarySchool = false;
-            else this.engine.Player.IsSuccessfulAtPrimarySchool = true;
+            if (this.engine.Player.IsTakingLessons) this.engine.Player.IsSuccessfulAtHighSchool = true;
+            else if (examsPercent < 50) this.engine.Player.IsSuccessfulAtHighSchool = false;
+            else this.engine.Player.IsSuccessfulAtHighSchool = true;
 
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("You have finished your Primary School.");
+            stringBuilder.AppendLine("You have finished your High School.");
             if (this.engine.Player.IsTakingLessons)
             {
                 stringBuilder.AppendLine($"You've passed your exams with ease, {this.engine.NumberGenerator.ChooseNumber(80, 100)} percent.");
-                this.engine.Player.IsSuccessfulAtPrimarySchool = true;
+                this.engine.Player.IsSuccessfulAtHighSchool = true;
             }
             else if (examsPercent < 50)
             {
                 stringBuilder.AppendLine($"You've passed your exams with approximately {examsPercent} percent.");
-                this.engine.Player.IsSuccessfulAtPrimarySchool = false;
+                this.engine.Player.IsSuccessfulAtHighSchool = false;
             }
             else
             {
                 stringBuilder.AppendLine($"You've passed your exams with approximately {examsPercent} percent.");
-                this.engine.Player.IsSuccessfulAtPrimarySchool = true;
+                this.engine.Player.IsSuccessfulAtHighSchool = true;
             }
             return stringBuilder.ToString();
         }
