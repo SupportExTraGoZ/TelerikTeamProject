60653c0c326dc22d77565e7e8387cad11d6c569c
Angel <angelmizinov@abv.bg>
2018-08-12 09:59:43 +0300

    [EducationalPicker]Add KinderGartenPicker and names of schools

--- LifeSim/LifeSim.Player/Randomizer/Models/EducationInstitutePicker.cs
+++ LifeSim/LifeSim.Player/Randomizer/Models/EducationInstitutePicker.cs
@@ -2,11 +2,18 @@
 using LifeSim.Player.Randomizer.Contracts;
 using System.Collections.Generic;
 using System.Linq;
+
 namespace LifeSim.Player.Randomizer.Models
 {
     public class EducationInstitutePicker : IEducationInstitutePicker
     {
         private readonly Random GetRandom = new Random();
+
+        public string PickKinderGarten(bool successful = false)
+        {
+            var tempStorage = kinderGartens.Keys.Where(x => kinderGartens[x] == successful);
+            return tempStorage.ElementAt(GetRandom.Next(0, tempStorage.Count() - 1));
+        }
 
         public string PickPrimarySchool(bool successful = false)
         {
@@ -26,6 +33,16 @@ namespace LifeSim.Player.Randomizer.Models
             return tempStorage.ElementAt(GetRandom.Next(0, tempStorage.Count() - 1));
         }
 
+        private readonly IDictionary<string, bool> kinderGartens = new Dictionary<string, bool>
+            {
+                // bool = Is school prestigious or not
+                {"Germantown Friends School",true },
+                {"Princeton Day School",true},
+                {"Collegiate School",false },
+                {"Horace Mann School",false },
+                {"Trinity School",true }
+            };
+
         private readonly IDictionary<string, bool> primarySchools = new Dictionary<string, bool>
             {
                 // bool = Is school prestigious or not
@@ -41,25 +58,25 @@ namespace LifeSim.Player.Randomizer.Models
         private readonly IDictionary<string, bool> highSchools = new Dictionary<string, bool>
             {
                 // bool = Is school prestigious or not
-                {"John Ward", true},
-                {"Edward Brooke-Charter", true},
-                {"Neshkoro Elementary", false},
-                {"Weyerhaeuser Elementary", true},
-                {"Life School McKinney", false},
-                {"St. Martin Elementary", false},
-                {"Polaris at Albert Einstein", true}
+                {"Scottsdale", true},
+                {"Chandler", true},
+                {"Oro Valley", false},
+                {"Tucson North", true},
+                {"Flagstaff", false},
+                {"Meridian School", false},
+                {"Thomas Jefferson", true}
             };
 
         private readonly IDictionary<string, bool> universities = new Dictionary<string, bool>
             {
                 // bool = Is school prestigious or not
-                {"John Ward", true},
-                {"Edward Brooke-Charter", true},
-                {"Neshkoro Elementary", false},
-                {"Weyerhaeuser Elementary", true},
-                {"Life School McKinney", false},
-                {"St. Martin Elementary", false},
-                {"Polaris at Albert Einstein", true}
+                {"Princeton University", true},
+                {"Harvard University", true},
+                {"Yale University", false},
+                {"Stanford University", true},
+                {"Georgetown University", true},
+                {"Cornell University", false},
+                {"Washington University in St. Louis", false}
             };
     }
 }
