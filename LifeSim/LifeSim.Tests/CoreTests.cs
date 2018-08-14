b3e732739cddc7a0b244e4b9953f5995a279695b
eXtraGoZ <stranged96@gmail.com>
2018-08-14 15:39:25 +0300

    Unit Test Setup

---
+++ LifeSim/LifeSim.Tests/CoreTests.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LifeSim.Player.Models;
+using LifeSim.Player.Randomizer.Models;
+
+namespace LifeSim.Tests
+{
+    [TestClass]
+    public class CoreTests
+    {
+        [TestMethod]
+        public void ApplyForJob_SoftwareEngineer()
+        {
+            // Arrange
+            var tempPlayer = new Player.Models.Player("Danail", "Grozdanov", Player.Enums.GenderType.Male, Player.Enums.Birthplaces.Miami, new FamilyGenerator());
+            tempPlayer.IsTakingLessons = true;
+            tempPlayer.IsSuccessfulAtUniversity = true;
+
+            // Act
+
+
+            // Assert
+        }
+    }
+}
