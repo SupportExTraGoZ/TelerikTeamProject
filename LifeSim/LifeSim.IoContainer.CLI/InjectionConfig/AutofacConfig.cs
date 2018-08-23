e52f3a80cbc6aa3c0edf8f7b937f23ee46562f3c
Stefan Veselinov <chofexx@gmail.com>
2018-08-23 20:10:52 +0300

    Base AutofacConfig implementation
    All implementations are taken from latest workshop, TAKE NOTE at the RegisterCoreComponents !! there must be build all registry!!

---
+++ LifeSim/LifeSim.IoContainer.CLI/InjectionConfig/AutofacConfig.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace LifeSim.IoContainer.CLI.InjectionConfig
+{
+    public class AutofacConfig : Autofac.Module
+    {
+        public IContainer Build()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            this.RegisterConvention(containerBuilder);
+            this.RegisterCoreComponents(containerBuilder);
+
+            return containerBuilder.Build();
+        }
+
+        private void RegisterConvention(ContainerBuilder builder)
+        {
+            var coreAssembly = Assembly.GetExecutingAssembly();
+
+           
+            builder.RegisterAssemblyTypes(coreAssembly)
+                    .AsImplementedInterfaces();
+        }
+
+        private void RegisterCoreComponents(ContainerBuilder builder)
+        {
+            //builder.RegisterType<FurnitureManufacturerEngine>().As<IEngine>().SingleInstance();
+            // Write the rest of your bindings... (if needed)
+        }
+
+    }
+}
