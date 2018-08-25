86d19f925a9e9294a234650bb45753b0dd7fe4a0
Stefan Veselinov <chofexx@gmail.com>
2018-08-25 10:24:06 +0300

    Adding to ICommand

--- LifeSim/LifeSim.Core/Engine/Commands/Contracts/ICommand.cs
+++ LifeSim/LifeSim.Core/Engine/Commands/Contracts/ICommand.cs
@@ -4,6 +4,10 @@ namespace LifeSim.Core.Engine.Commands.Contracts
 {
     public interface ICommand
     {
+        string Name { get; }
+
+        IList<string> Parameters(string commandParameters);
+
         string Execute(IList<string> parameters);
     }
 }
