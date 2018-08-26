d27be4edb5f4343372ddca029c734902a90c7057
Stefan Veselinov <chofexx@gmail.com>
2018-08-26 09:24:03 +0300

    Adding Unit test folders structure
    making UT- folder structure simular to the project file structure

---
+++ LifeSim/LifeSim.Tests/LifeSimCoreTests/EngineTests/MenuTests/ManagerTests/ModelsTests/Constructor_Should.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LifeSim.Tests.LifeSimCoreTests.EngineTests.MenuTests.ManagerTests.ModelsTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void Test1()
+        {
+        }
+    }
+}
