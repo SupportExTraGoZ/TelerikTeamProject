36c5a235e2a341acb86b168b2e388cfaba7cc50a
Stefan Veselinov <chofexx@gmail.com>
2018-08-28 07:03:10 +0300

    Adding test to UserInteraction AddAction

---
+++ LifeSim/LifeSim.Tests/LifeSimCoreTests/EngineTests/ConsoleManagerTests/UserInteraction_AddAction_Should.cs
@@ -0,0 +1,28 @@
+﻿using LifeSim.Core.CLI.Module.ConsoleManagement.Contracts;
+using LifeSim.Core.CLI.Module.ConsoleManagement.Functions.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeSim.Tests.LifeSimCoreTests.EngineTests.ConsoleManagerTests
+{
+    [TestClass]
+    public class UserInteraction_AddAction_Should
+    {
+        [TestMethod]
+        public void ShouldInsertToCollection_WhenStringIsNotNull()
+        {
+            var consoleWriterMock = new Mock<IConsoleWriter>();
+       
+            var consoleReaderMock = new Mock<IConsoleReader>();
+
+            var userInteraction = new UserInteraction(consoleWriterMock.Object, consoleReaderMock.Object);
+            userInteraction.AddAction("action");
+
+            Assert.IsTrue(userInteraction.ActionLog.Contains("action"));
+        }
+
+    }
+}
